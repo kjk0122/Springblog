21e2ed20ab2de4c79380abc2d7922cc279749767
kjk0122 <juke9548@gmail.com>
2023-04-19 17:11:55 +0900

    쩝

---
+++ src/test/java/com/example/kjkspringblog/entity/UserTest.java
@@ -0,0 +1,17 @@
+package com.example.kjkspringblog.entity;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+@Test
+@DisplayName("정상 케이스")
+void createUser(){
+   long id = 100l;
+   String username ="wewew9e";
+   String password = "32r32rq!";
+    UserRoleEnum role=UserRoleEnum.USER;//테스트 하다 되돌림;;
+}
+}
