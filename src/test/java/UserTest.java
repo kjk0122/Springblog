f3cd5c32627f6ee84374c58f80544770a8bddb1c
kjk0122 <juke9548@gmail.com>
2023-04-29 22:54:52 +0900

    취소

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -8,9 +8,5 @@ class UserTest {
 @Test
 @DisplayName("정상 케이스")
 void createUser(){
-   long id = 100l;
-   String username ="wewew9e";
-   String password = "32r32rq!";
-    UserRoleEnum role=UserRoleEnum.USER;//테스트 하다 되돌림;;
 }
 }
