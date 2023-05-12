9d3be0254f360cf526b67f33ba76404c8499772e
kjk0122 <juke9548@gmail.com>
2023-05-12 17:31:17 +0900

    오류 메세지 추가

--- src/main/java/com/example/kjkspringblog/exception/RestApiExceptionHandler.java
+++ src/main/java/com/example/kjkspringblog/exception/RestApiExceptionHandler.java
@@ -1,22 +1,50 @@
 package com.example.kjkspringblog.exception;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 public class RestApiExceptionHandler {
 
-    @ExceptionHandler(value = { Exception.class })
-    public ResponseEntity<Object> handleApiRequestException(Exception ex) {
-        RestApiException restApiException = new RestApiException();
-        restApiException.setHttpStatus(HttpStatus.BAD_REQUEST.value());
-        restApiException.setErrorMessage(ex.getMessage());
+    @ExceptionHandler({NoSuchElementException.class, EmptyResultDataAccessException.class})
+    public ResponseEntity<String> handleNoSuchElementException(Exception e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
 
-        return new ResponseEntity<>(
-                restApiException,
-                HttpStatus.BAD_REQUEST
-        );
+    @ExceptionHandler(HttpClientErrorException.NotFound.class)
+    public ResponseEntity<String> handleNotFoundException(Exception e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+    @ExceptionHandler({IllegalArgumentException.class})
+    public ResponseEntity<String> handleBadRequestException(Exception e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler({RuntimeException.class})
+    public ResponseEntity<String> handleRuntimeException(Exception e) {
+        return ResponseEntity.internalServerError().body(e.getMessage());
+    }
+    @ExceptionHandler({InvalidFormatException.class})
+    protected ResponseEntity<String> handleInvalidFormatException(Exception e) {
+        return ResponseEntity.unprocessableEntity().body(e.getMessage());
+    }
+
+    @ExceptionHandler({ConflictException.class})
+    public ResponseEntity<String> handleConflictException(Exception e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    }
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public static class ConflictException extends RuntimeException {
+        public ConflictException(String message) {
+            super(message);
+        }
     }
 }
